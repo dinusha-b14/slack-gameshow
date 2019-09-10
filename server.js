64a41c26d804a50147701214e7bec6c59d87770d
Dinusha (Dan) Bodhinayake <dan.bodhinayake@pwc.com>
2019-09-10 11:17:35 +1000

    some setup for gcloud app deploy

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ const { port } = require('./lib/config');
 const app = require('./src/app');
 
 app.listen(port, () => console.log(`Gameshow app listening on port ${port}`));
+
+module.exports = app;
